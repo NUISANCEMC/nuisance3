e34974c1435e8e8bc53cda65a97c845b7cc2c8be
patrickstowell <p.stowell@sheffield.ac.uk>
2024-03-04 20:12:09 +0000

    Moved to templated argument calls in compframe

--- src/nuis/record/ComparisonFrame.h
+++ src/nuis/record/ComparisonFrame.h
@@ -2,12 +2,11 @@
 
 #include "nuis/histframe/Binning.h"
 #include "nuis/histframe/HistFrame.h"
-
+#include <utility>
 #include <string>
 
+
 namespace nuis {
-// Actually I think we do need inheritance here
-// Then we can just do comparison["data"].content
 struct ComparisonFrame {
 
     std::string normalisation_type;
@@ -20,31 +19,52 @@ struct ComparisonFrame {
     HistFrame mc;
     HistFrame data;
 
-    // Here its a bit unclear how to make the 0 column data...
     ComparisonFrame(Bins::BinOp bindef) {
         mc = HistFrame(bindef, "mc");
         data = HistFrame(bindef, "data");
     }
 
-    // I don't really like this. Need to redict most fill commands to mc.
-    column_t add_column(std::string const &name,
-        std::string const &label = "") {
-        return mc.add_column(name, label);
-    }
+    // Redirect most function calls down to mc as data is static
+    template<typename... TS>
+    auto add_column(TS&&... args)
+        {return mc.add_column(std::forward<TS>(args)...);};
 
-    column_t find_column_index(std::string const &name) const {
-        return mc.find_column_index(name);
-    }
+    template<typename... TS>
+    auto find_column_index(TS&&... args)
+        {return mc.find_column_index(std::forward<TS>(args)...);};
 
-    Eigen::ArrayXd get_content(column_t col = 0,
-                                bool divide_by_bin_sizes = false) const {
-        return mc.get_content(col, divide_by_bin_sizes);
-    }
+    template<typename... TS>
+    Eigen::ArrayXd get_content(TS&&... args)
+        {return mc.get_content(std::forward<TS>(args)...);};
 
-    Eigen::ArrayXd get_error(column_t col = 0,
-                            bool divide_by_bin_sizes = false) const {
-        return mc.get_error(col, divide_by_bin_sizes);
-    }
+    template<typename... TS>
+    Eigen::ArrayXd get_error(TS&&... args)
+        {return mc.get_error(std::forward<TS>(args)...);};
+
+    template<typename... TS>
+    void fill(TS&&... args)
+        {return mc.fill(std::forward<TS>(args)...);};
+
+    template<typename... TS>
+    void fill_with_selection(TS&&... args)
+        {return mc.fill_with_selection(std::forward<TS>(args)...);};
+
+    template<typename... TS>
+    auto find_bin(TS&&... args)
+        {return mc.find_bin(std::forward<TS>(args)...);};
+
+    template<typename... TS>
+    void reset(TS&&... args)
+        {return mc.reset(std::forward<TS>(args)...);};
+
+    // Add some data functions
+    template<typename... TS>
+    Eigen::ArrayXd get_data_content(TS&&... args)
+        {return data.get_content(std::forward<TS>(args)...);};
+
+    template<typename... TS>
+    Eigen::ArrayXd get_data_error(TS&&... args)
+        {return data.get_error(std::forward<TS>(args)...);};
 
     HistColumn_View operator[](std::string const &name) const {
         if (name == "data") return data[name];
@@ -56,24 +76,6 @@ struct ComparisonFrame {
         return mc[colid];
     }
 
-    Bins::BinId find_bin(std::vector<double> const &projections) const {
-        return mc.find_bin(projections);
-    }
-
-    void fill(std::vector<double> const &projections, double weight,
-                column_t col = 0) {
-        mc.fill(projections, weight, col);
-    }
-
-    void fill_with_selection(int sel_int,
-        std::vector<double> const &projections,
-        double weight, column_t col = 0) {
-        mc.fill_with_selection(sel_int, projections, weight, col);
-    }
-
-    void reset(){
-        mc.reset();
-    }
 };
 }
 
