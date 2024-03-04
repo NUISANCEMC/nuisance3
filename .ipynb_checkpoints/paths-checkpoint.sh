b0fef074f97b39249ff5bd041b340e2d7f72bd7c
patrickstowell <p.stowell@sheffield.ac.uk>
2024-03-04 20:26:57 +0000

    Remove ipynb checkpoints

--- .ipynb_checkpoints/paths-checkpoint.sh
+++
@@ -1,1 +0,0 @@
-export CPLUS_INCLUDE_PATH=$NUISANCE_ROOT/include/:$NUISANCE_ROOT/../src/:/opt/HepMC3/git_master/include/:/opt/NuHepMC_CPPUtils/git_master/include/:/opt/ProSelecta/git_master/include/:/opt/genie/3_04_00/include/:/opt/lhapdf/5.9.1/include/:/opt/neut/git_master/include/:/opt/pybind11/v2.9.2/include/:/opt/root/v6-28-08/include/:/opt/yaml-cpp/v0.8.0/include/:$CPLUS_INCLUDE_PATH
