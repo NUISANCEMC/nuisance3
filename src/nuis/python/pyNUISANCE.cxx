486822c857624fe01beb1737ed6c5c231ac24268
Luke Pickering <luketpickering@gmail.com>
2024-03-05 09:22:39 +0000

    removes make_opaque for stl types to improve ergnomics of python interface

--- src/nuis/python/pyNUISANCE.cxx
+++ src/nuis/python/pyNUISANCE.cxx
@@ -20,11 +20,6 @@
 
 namespace py = pybind11;
 
-PYBIND11_MAKE_OPAQUE(std::vector<bool>);
-PYBIND11_MAKE_OPAQUE(std::vector<int>);
-PYBIND11_MAKE_OPAQUE(std::vector<double>);
-PYBIND11_MAKE_OPAQUE(std::vector<uint32_t>);
-
 PYBIND11_MODULE(pyNUISANCE, m) {
   m.doc() = "NUISANCE implementation in python";
 
@@ -39,11 +34,6 @@ PYBIND11_MODULE(pyNUISANCE, m) {
   auto pps = py::module::import("pyProSelecta");
   m.add_object("pps", pps);
   m.add_object("pyProSelecta", pps);
-
-  py::bind_vector<std::vector<bool>>(m, "Vector_bool");
-  py::bind_vector<std::vector<int>>(m, "Vector_int");
-  py::bind_vector<std::vector<double>>(m, "Vector_double");
-  py::bind_vector<std::vector<uint32_t>>(m, "Vector_uint32_t");
 
   init_eventinput(m);
   init_frame(m);
