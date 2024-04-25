9abe776f2c9eef7f38e4f716cbce89ea48fcd15a
Luke Pickering <luketpickering@gmail.com>
2024-04-25 17:22:51 +0000

    Adds hepdata reference resolver and separates eventframe filler code/binding

---
+++ src/nuis/histframe/fill_from_EventFrame.h
@@ -0,0 +1,121 @@
+#pragma once
+
+#include "nuis/histframe/HistFrame.h"
+
+#include "nuis/eventframe/EventFrameGen.h"
+
+namespace nuis {
+
+void fill_from_EventFrame(
+    HistFrame &hf, EventFrame const &ef,
+    std::vector<std::string> const &projection_column_names,
+    std::vector<std::string> const &weight_column_names = {"weight.cv"});
+
+void fill_from_EventFrame_if(
+    HistFrame &hf, EventFrame const &ef,
+    std::string const &conditional_column_name,
+    std::vector<std::string> const &projection_column_names,
+    std::vector<std::string> const &weight_column_names = {"weight.cv"});
+
+void fill_columns_from_EventFrame(
+    HistFrame &hf, EventFrame const &ef,
+    std::vector<std::string> const &projection_column_names,
+    std::string const &column_selector_column_name,
+    std::vector<std::string> const &weight_column_names = {"weight.cv"});
+
+void fill_columns_from_EventFrame_if(
+    HistFrame &hf, EventFrame const &ef,
+    std::string const &conditional_column_name,
+    std::vector<std::string> const &projection_column_names,
+    std::string const &column_selector_column_name,
+    std::vector<std::string> const &weight_column_names = {"weight.cv"});
+
+void fill_weighted_columns_from_EventFrame(
+    HistFrame &hf, EventFrame const &ef,
+    std::vector<std::string> const &projection_column_names,
+    std::vector<std::string> const &column_weighter_names,
+    std::vector<std::string> const &weight_column_names = {"weight.cv"});
+
+void fill_weighted_columns_from_EventFrame_if(
+    HistFrame &hf, EventFrame const &ef,
+    std::string const &conditional_column_name,
+    std::vector<std::string> const &projection_column_names,
+    std::vector<std::string> const &column_weighter_names,
+    std::vector<std::string> const &weight_column_names = {"weight.cv"});
+
+void fill_procid_columns_from_EventFrame(
+    HistFrame &hf, EventFrame const &ef,
+    std::vector<std::string> const &projection_column_names,
+    std::vector<std::string> const &weight_column_names = {"weight.cv"});
+
+void fill_procid_columns_from_EventFrame_if(
+    HistFrame &hf, EventFrame const &ef,
+    std::string const &conditional_column_name,
+    std::vector<std::string> const &projection_column_names,
+    std::vector<std::string> const &weight_column_names = {"weight.cv"});
+
+void fill_from_EventFrameGen(
+    HistFrame &hf, EventFrameGen &efg,
+    std::vector<std::string> const &projection_column_names,
+    std::vector<std::string> const &weight_column_names = {"weight.cv"});
+
+#ifdef NUIS_ARROW_ENABLED
+template <typename ArrTabular = arrow::RecordBatch>
+void fill_from_Arrow(HistFrame &hf, std::shared_ptr<ArrTabular> const &tab,
+                     std::vector<std::string> const &projection_column_names,
+                     std::vector<std::string> const &weight_column_names = {
+                         "weight.cv"});
+
+template <typename ArrTabular = arrow::RecordBatch>
+void fill_from_Arrow_if(HistFrame &hf, std::shared_ptr<ArrTabular> const &tab,
+                        std::string const &conditional_column_name,
+                        std::vector<std::string> const &projection_column_names,
+                        std::vector<std::string> const &weight_column_names = {
+                            "weight.cv"});
+
+template <typename ArrTabular = arrow::RecordBatch>
+void fill_columns_from_Arrow(
+    HistFrame &hf, std::shared_ptr<ArrTabular> const &tab,
+    std::vector<std::string> const &projection_column_names,
+    std::string const &column_selector_column_name,
+    std::vector<std::string> const &weight_column_names = {"weight.cv"});
+
+template <typename ArrTabular = arrow::RecordBatch>
+void fill_columns_from_Arrow_if(
+    HistFrame &hf, std::shared_ptr<ArrTabular> const &tab,
+    std::string const &conditional_column_name,
+    std::vector<std::string> const &projection_column_names,
+    std::string const &column_selector_column_name,
+    std::vector<std::string> const &weight_column_names = {"weight.cv"});
+
+template <typename ArrTabular = arrow::RecordBatch>
+void fill_weighted_columns_from_Arrow(
+    HistFrame &hf, std::shared_ptr<ArrTabular> const &tab,
+    std::vector<std::string> const &projection_column_names,
+    std::vector<std::string> const &column_weighter_names,
+    std::vector<std::string> const &weight_column_names = {"weight.cv"});
+
+template <typename ArrTabular = arrow::RecordBatch>
+void fill_weighted_columns_from_Arrow_if(
+    HistFrame &hf, std::shared_ptr<ArrTabular> const &tab,
+    std::string const &conditional_column_name,
+    std::vector<std::string> const &projection_column_names,
+    std::vector<std::string> const &column_weighter_names,
+    std::vector<std::string> const &weight_column_names = {"weight.cv"});
+
+template <typename ArrTabular = arrow::RecordBatch>
+void fill_procid_columns_from_Arrow(
+    HistFrame &hf, std::shared_ptr<ArrTabular> const &tab,
+    std::vector<std::string> const &projection_column_names,
+    std::vector<std::string> const &weight_column_names = {"weight.cv"});
+
+template <typename ArrTabular = arrow::RecordBatch>
+void fill_procid_columns_from_Arrow_if(
+    HistFrame &hf, std::shared_ptr<ArrTabular> const &tab,
+    std::string const &conditional_column_name,
+    std::vector<std::string> const &projection_column_names,
+    std::vector<std::string> const &weight_column_names = {"weight.cv"});
+
+#endif
+
+} // namespace nuis
--- src/nuis/python/modules/__init__.py
+++ src/nuis/python/modules/__init__.py
@@ -1,5 +1,93 @@
 from ._pyNUISANCE import *
 
+try:
+  import requests
+  from .HepDataRefResolver import GetLocalPathToResource
+  import os
+  import types
+  import yaml
+  import pyProSelecta as pps
+
+  hepdata = types.SimpleNamespace()
+
+  def _get_local_path_to_resource(ref, *args, **context):
+
+    record_database_root = os.environ.get("NUISANCE_RECORD_DATABASE")
+
+    if not record_database_root:
+      raise RuntimeError("NUISANCE_RECORD_DATABASE environment variable is not defined")
+
+    return GetLocalPathToResource(record_database_root, ref, **context)
+
+  def _get_independent_variables(table):
+    return [ x["header"]["name"] for x in table["independent_variables"] ]
+  
+  def _get_dependent_variables(table):
+    return [ x["header"]["name"] for x in table["dependent_variables"] ]
+
+  def _get_qualifiers(table, dependent_variable=0):
+    if isinstance(dependent_variable, str):
+      dependent_variable = _get_dependent_variables(table).index(dependent_variable)
+    return { kv["name"]:kv["value"] for kv in table["dependent_variables"][dependent_variable]["qualifiers"] }
+
+  def _get_tables(submission_path):
+    tables = {}
+
+    with open("/".join([submission_path, "submission.yaml"]), 'r') as file:
+      submission_yaml = yaml.safe_load_all(file)
+    
+      # loop through sub-documents in the submission file and load up all referenced tables
+      for doc in submission_yaml:
+          if "data_file" in doc:
+              df = doc["data_file"]
+              with open("/".join([submission_path, df]), 'r') as datafile:
+                  tables[df[0:-5]] = yaml.safe_load(datafile)
+    return tables
+
+  def _get_selection_function(table, dependent_variable=0):
+    quals = _get_qualifiers(table, dependent_variable)
+    return (quals["select"], pps.select.get(quals["select"]))
+
+  def _get_projection_functions(table, dependent_variable=0):
+    quals = _get_qualifiers(table, dependent_variable)
+    project_names = [ quals["project:%s" % v] for v in _get_independent_variables(table) ]
+
+    return (project_names, [ pps.project.get(fn) for fn in project_names ])
+
+  def _make_BinnedValues(table):
+    return convert.HistFrame.from_yaml_str(str(table))
+
+  def _get_referenced_table(ref, *args, **context):
+    local_path = _get_local_path_to_resource(ref, **context)
+    with open(local_path) as datafile:
+      return yaml.safe_load(datafile)
+    raise RuntimeError("Failed to parse %s at yaml document" % local_path)
+
+  def _get_sub_measurement_tables(table, dependent_variable=0, *args, **context):
+    quals = _get_qualifiers(table, dependent_variable)
+    return [ _get_referenced_table(m, **context) for m in quals["sub_measurements"].split(",") ]
+
+  def _get_covariance(table, dependent_variable=0, *args, **context):
+    quals = _get_qualifiers(table, dependent_variable)
+    local_path = _get_local_path_to_resource(quals["covariance"], **context)
+    with open(local_path) as datafile:
+      return convert.Covariance.from_yaml_str(str(yaml.safe_load(datafile)))
+
+  hepdata.get_local_path_to_resource = _get_local_path_to_resource
+  hepdata.get_independent_variables = _get_independent_variables
+  hepdata.get_dependent_variables = _get_dependent_variables
+  hepdata.get_selection_function = _get_selection_function
+  hepdata.get_projection_functions = _get_projection_functions
+  hepdata.get_qualifiers = _get_qualifiers
+  hepdata.get_sub_measurement_tables = _get_sub_measurement_tables
+  hepdata.get_covariance = _get_covariance
+  hepdata.get_tables = _get_tables
+  hepdata.get_referenced_table = _get_referenced_table
+  hepdata.make_BinnedValues = _make_BinnedValues
+
+except ModuleNotFoundError:
+    pass
+
 # Matplotlib interfaces
 try:
     import matplotlib.pyplot as plt
