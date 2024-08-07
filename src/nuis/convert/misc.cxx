36a427969888cf5a7702fa7e6198d605c4a6fed5
Luke Pickering <luketpickering@gmail.com>
2024-08-07 12:49:06 +0100

    remove boost json. also ends up removing plotly helpers that weren't used
    - if we lean hard into plotly in the future, we can get them back.

--- src/nuis/convert/misc.cxx
+++ src/nuis/convert/misc.cxx
@@ -2,39 +2,10 @@
 
 #include "nuis/binning/utility.h"
 
-#include "boost/json.hpp"
-
 #include "fmt/core.h"
 #include "fmt/ranges.h"
 
 namespace nuis {
-
-std::string to_plotly1D(BinnedValuesBase const &bvb) {
-  boost::json::array data;
-
-  auto values = bvb.get_bin_contents();
-
-  for (BinnedValuesBase::column_t col = 0; col < bvb.column_info.size();
-       ++col) {
-    boost::json::object data_instance;
-    data_instance["type"] = "scatter";
-
-    boost::json::array x;
-    auto bcs = get_bin_centers1D(bvb.binning->bins);
-    std::copy(bcs.begin(), bcs.end(), std::back_inserter(x));
-    data_instance["x"] = x;
-
-    boost::json::array col_contents;
-
-    std::copy(values.col(col).begin(), values.col(col).end(),
-              std::back_inserter(col_contents));
-
-    data_instance["y"] = col_contents;
-    data.push_back(data_instance);
-  }
-
-  return boost::json::serialize(data);
-}
 
 std::tuple<Eigen::ArrayXXd, Eigen::ArrayXXd, Eigen::ArrayXXd>
 to_mpl_pcolormesh(BinnedValuesBase const &bvb, BinnedValuesBase::column_t col) {
--- src/nuis/convert/misc.h
+++ src/nuis/convert/misc.h
@@ -8,7 +8,6 @@
 #include <string>
 
 namespace nuis {
-std::string to_plotly1D(BinnedValuesBase const &);
 std::tuple<Eigen::ArrayXXd, Eigen::ArrayXXd, Eigen::ArrayXXd>
 to_mpl_pcolormesh(BinnedValuesBase const &, BinnedValuesBase::column_t col = 0);
 } // namespace nuis
