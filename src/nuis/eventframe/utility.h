0fe454d808a33639ed414f6b0f729c2966b79e85
Luke Pickering <luketpickering@gmail.com>
2024-10-11 17:57:26 +0100

    separate out the api for getting the FATX in a given unit from a frame

---
+++ src/nuis/eventframe/utility.h
@@ -0,0 +1,91 @@
+#pragma once
+
+#include "nuis/eventframe/EventFrame.h"
+#include "nuis/eventframe/column_types.h"
+
+#include "NuHepMC/UnitsUtils.hxx"
+
+#ifdef NUIS_ARROW_ENABLED
+#include "arrow/api.h"
+#endif
+
+#include "spdlog/fmt/bundled/core.h"
+
+#include <map>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+namespace nuis {
+
+template <typename EFT>
+double get_best_fatx_per_sumw_estimate(
+    EFT const &ef, NuHepMC::CrossSection::Units::Unit const &xs_units) {
+
+  std::string fatx_colname;
+
+  if (xs_units.tgtscale ==
+      NuHepMC::CrossSection::Units::TargetScale::PerTarget) {
+    fatx_colname = "fatx_per_sumw.pb_per_target.estimate";
+  } else if (xs_units.tgtscale ==
+             NuHepMC::CrossSection::Units::TargetScale::PerTargetNucleon) {
+    fatx_colname = "fatx_per_sumw.pb_per_nucleon.estimate";
+  } else {
+    std::stringstream ss;
+    ss << xs_units.tgtscale;
+    throw std::runtime_error(
+        fmt::format("When retrieving best fatx_per_sumw estimate from "
+                    "proferred event frame, the analysis target "
+                    "scale was: {}, which is invalid for automatic scaling.",
+                    ss.str()));
+  }
+
+  static std::map<NuHepMC::CrossSection::Units::Scale, double> const
+      xsunit_factors = {
+          {NuHepMC::CrossSection::Units::Scale::pb,
+           NuHepMC::CrossSection::Units::pb},
+          {NuHepMC::CrossSection::Units::Scale::cm2,
+           NuHepMC::CrossSection::Units::cm2},
+          {NuHepMC::CrossSection::Units::Scale::cm2_ten38,
+           NuHepMC::CrossSection::Units::cm2_ten38},
+      };
+
+  double units_scale =
+      NuHepMC::CrossSection::Units::pb / xsunit_factors.at(xs_units.scale);
+
+  if constexpr (std::is_same_v<EFT, EventFrame>) {
+    return ef.col(fatx_colname).tail(1)(0) * units_scale;
+  }
+#ifdef NUIS_ARROW_ENABLED
+  else if constexpr (std::is_same_v<EFT, std::shared_ptr<arrow::RecordBatch>>) {
+    if (!ef->num_rows()) {
+      throw std::runtime_error(
+          "in SingleDistributionAnalysis::process was passed an arrow "
+          "record batch with no rows.");
+    }
+    return get_col_cast_to<double>(ef, fatx_colname)(ef->num_rows() - 1) *
+           units_scale;
+  } else if constexpr (std::is_same_v<EFT, std::shared_ptr<arrow::Table>>) {
+
+    double best_estimate = 0;
+    for (auto rb : arrow::TableBatchReader(ef)) {
+      if (!rb.ValueOrDie()->num_rows()) {
+        break;
+      }
+      best_estimate = get_col_cast_to<double>(rb.ValueOrDie(), fatx_colname)(
+                          rb.ValueOrDie()->num_rows() - 1) *
+                      units_scale;
+    }
+
+    if (best_estimate == 0) {
+      throw std::runtime_error(
+          "in SingleDistributionAnalysis::process was passed an "
+          "arrow table with no rows");
+    }
+
+    return best_estimate;
+  }
+#endif
+}
+
+} // namespace nuis
