6b5cc7a56fe200f0ed3dff236b2553ef4cda4a06
patrickstowell <p.stowell@sheffield.ac.uk>
2024-11-12 10:00:56 +0000

    Fix to HepMC3 min version. Added pyunnormalized .

--- src/nuis/python/pyEventInput.cxx
+++ src/nuis/python/pyEventInput.cxx
@@ -83,6 +83,83 @@ IEventSource_sentinel end(pyNormalizedEventSource &) {
   return IEventSource_sentinel();
 }
 
+
+
+// Unnormalized bindings
+
+pyEventSource::pyEventSource(std::string const &filename) {
+  auto resp = fact.make_unnormalized(filename);
+  gri = resp.first;
+  evs = resp.second;
+}
+
+pyEventSource::pyEventSource(YAML::Node const &node) {
+  auto resp = fact.make_unnormalized(node);
+  gri = resp.first;
+  evs = resp.second;
+}
+
+py::object pyEventSource::first() {
+  if (!evs) {
+    return py::none();
+  }
+  auto evt = evs->first();
+  curr_event = py::make_tuple(evt, 1.0);
+  return curr_event;
+}
+
+py::object pyEventSource::next() {
+  if (!evs) {
+    return py::none();
+  }
+  auto evt = evs->next();
+
+  // PS : Needed this to fix event loop breaking issue
+  if (!evt) {
+    return py::none();
+  }
+  curr_event = py::make_tuple(evt, 1.0);
+  return curr_event;
+}
+
+std::shared_ptr<HepMC3::GenRunInfo> pyEventSource::run_info() {
+  return gri;
+}
+
+bool pyEventSource::good() { return bool(evs); }
+
+pyEventSource_looper::pyEventSource_looper(
+    pyEventSource &pyevs)
+    : pysource(pyevs) {
+  curr_event = pysource.get().first();
+}
+
+void pyEventSource_looper::operator++() {
+  curr_event = pysource.get().next();
+}
+
+py::object const &pyEventSource_looper::operator*() {
+  return curr_event;
+}
+bool pyEventSource_looper::operator!=(
+    IEventSource_sentinel const &) const {
+  return !curr_event.is(py::none());
+}
+
+bool pyEventSource_looper::operator==(
+    IEventSource_sentinel const &) const {
+  return curr_event.is(py::none());
+}
+
+pyEventSource_looper begin(pyEventSource &evs) {
+  return pyEventSource_looper(evs);
+}
+
+IEventSource_sentinel end(pyEventSource &) {
+  return IEventSource_sentinel();
+}
+
+
 void pyEventInputInit(py::module &m) {
 
   py::class_<NormInfo>(m, "NormInfo")
@@ -106,4 +183,20 @@ void pyEventInputInit(py::module &m) {
             return py::make_iterator(begin(s), end(s));
           },
           py::keep_alive<0, 1>());
+
+  py::class_<pyEventSource>(m, "UnNormalizedEventSource")
+      .def(py::init<std::string const &>())
+      .def(py::init<YAML::Node const &>())
+      .def("first", &pyEventSource::first)
+      .def("next", &pyEventSource::next)
+      .def("run_info", &pyEventSource::run_info)
+      .def("__bool__", &pyEventSource::good)
+      .def(
+          "__iter__",
+          [](pyEventSource &s) {
+            return py::make_iterator(begin(s), end(s));
+          },
+          py::keep_alive<0, 1>());
+
+      
 }
--- src/nuis/python/pyEventInput.h
+++ src/nuis/python/pyEventInput.h
@@ -35,4 +35,40 @@ public:
 pyNormalizedEventSource_looper begin(pyNormalizedEventSource &evs);
 nuis::IEventSource_sentinel end(pyNormalizedEventSource &);
 
+
+
+
+// Unnormalized handlers
+struct pyEventSource {
+  nuis::EventSourceFactory fact;
+  std::shared_ptr<HepMC3::GenRunInfo> gri;
+  nuis::IEventSourcePtr evs;
+  pybind11::tuple curr_event;
+
+  pyEventSource(std::string const &filename);
+  pyEventSource(YAML::Node const &node);
+
+  pybind11::object first();
+  pybind11::object next();
+  std::shared_ptr<HepMC3::GenRunInfo> run_info();
+  bool good();
+};
+
+class pyEventSource_looper {
+  std::reference_wrapper<pyEventSource> pysource;
+  pybind11::object curr_event;
+
+public:
+  pyEventSource_looper(pyEventSource &pyevs);
+  void operator++();
+  pybind11::object const &operator*();
+  bool operator!=(nuis::IEventSource_sentinel const &) const;
+  bool operator==(nuis::IEventSource_sentinel const &) const;
+};
+
+
+pyEventSource_looper begin(pyEventSource &evs);
+nuis::IEventSource_sentinel end(pyEventSource &);
+
+
 void pyEventInputInit(pybind11::module &m);
