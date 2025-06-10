d95b3d4f0bdc2b7184ff4e62f7cf9d21cd5c9b58
Luke Pickering <luketpickering@gmail.com>
2025-06-10 10:38:47 +0100

    Updates some ROOT histogram conversion functions

--- src/nuis/convert/ROOT.h
+++ src/nuis/convert/ROOT.h
@@ -4,6 +4,7 @@
 #include "nuis/binning/utility.h"
 
 #include "nuis/histframe/BinnedValues.h"
+#include "nuis/histframe/exceptions.h"
 #include "nuis/histframe/utility.h"
 
 #include "nuis/eventframe/missing_datum.h"
@@ -30,6 +31,13 @@ template <typename BV>
 std::unique_ptr<TH1> ToTH1(BV const &hf, std::string const &name,
                            bool divide_by_bin_width,
                            BinnedValuesBase::column_t col = 0) {
+
+  if (col >= hf.column_info.size()) {
+    throw InvalidColumnAccess()
+        << "In ToTH1, tried to access column: " << col << ", but only "
+        << hf.column_info.size() << " columns exist.";
+  }
+
   auto hfp = Project(hf, 0);
   auto bins = get_bin_edges1D(hfp.binning->bins, 0);
 
@@ -70,8 +78,15 @@ std::unique_ptr<TH1> ToTH1(BV const &hf, std::string const &name,
 template <typename BV>
 std::unique_ptr<TH2> ToTH2(BV const &hf, std::string const &name,
                            bool divide_by_bin_area,
-                           BinnedValuesBase::column_t col = 1) {
-  auto hfp = Project(hf, {0, 1});
+                           BinnedValuesBase::column_t col = 0) {
+
+  if (col >= hf.column_info.size()) {
+    throw InvalidColumnAccess()
+        << "In ToTH1, tried to access column: " << col << ", but only "
+        << hf.column_info.size() << " columns exist.";
+  }
+
+  auto hfp = Project(hf, std::vector<size_t>{0, 1});
 
   auto binsx = get_bin_edges1D(hfp.binning->bins, 0);
   auto binsy = get_bin_edges1D(hfp.binning->bins, 1);
@@ -86,20 +101,21 @@ std::unique_ptr<TH2> ToTH2(BV const &hf, std::string const &name,
     bin_scales = hfp.binning->bin_sizes();
   }
 
-  for (size_t bix = 0; bix < binsx.size(); ++bix) {
-    for (size_t biy = 0; biy < binsy.size(); ++biy) {
+  Eigen::ArrayXd contents = hfp.get_bin_contents().col(col) / bin_scales;
+  Eigen::ArrayXd error = hfp.get_bin_uncertainty().col(col) / bin_scales;
 
-      Binning::BinExtents bin{{binsx[bix], binsy[biy]}};
+  for (size_t bix = 1; bix < binsx.size(); ++bix) {
+    for (size_t biy = 1; biy < binsy.size(); ++biy) {
 
-      auto bi_it =
-          std::find(hfp.binning->bins.begin(), hfp.binning->bins.end(), bin);
+      auto bi = hfp.binning->find_bin({(binsx[bix - 1] + binsx[bix]) / 2.0,
+                                       (binsy[biy - 1] + binsy[biy]) / 2.0});
 
-      if (bi_it == hfp.binning->bins.end()) {
+      if (bi == Binning::npos) {
         std::stringstream ss;
         ss << fmt::format(
                   "[ToTH2]: When looking for bin id [{},{}] = ({},{}), ({},{})",
-                  bix, biy, binsx[bix], binsx[bix + 1], binsy[biy],
-                  binsy[biy + 1])
+                  bix - 1, biy - 1, binsx[bix - 1], binsx[bix], binsy[biy - 1],
+                  binsy[biy])
            << ". Failed to find it.\nInput hist:" << hf
            << "\n  - Projected binning: " << hfp.binning->bins
            << "\n  - bin edges x = " << fmt::format("{}", binsx)
@@ -108,19 +124,75 @@ std::unique_ptr<TH2> ToTH2(BV const &hf, std::string const &name,
         throw CatastrophicBinningFailure();
       }
 
-      auto bi = std::distance(hfp.binning->bins.begin(), bi_it);
-
-      root_hist->SetBinContent(
-          bix + 1, biy + 1, hfp.get_bin_contents()(bi, col) / bin_scales(bi));
-      root_hist->SetBinError(
-          bix + 1, biy + 1,
-          std::sqrt(hfp.get_bin_uncertainty()(bi, col) / bin_scales(bi)));
+      root_hist->SetBinContent(bix, biy, contents(bi));
+      root_hist->SetBinError(bix, biy, error(bi));
     }
   }
   return root_hist;
 }
 
-std::unique_ptr<TH3> ToTH3(HistFrame const &hf) { return nullptr; }
+template <typename BV>
+std::unique_ptr<TH3> ToTH3(BV const &hf, std::string const &name,
+                           bool divide_by_bin_area,
+                           BinnedValuesBase::column_t col = 0) {
+
+  if (col >= hf.column_info.size()) {
+    throw InvalidColumnAccess()
+        << "In ToTH1, tried to access column: " << col << ", but only "
+        << hf.column_info.size() << " columns exist.";
+  }
+
+  auto hfp = Project(hf, std::vector<size_t>{0, 1, 2});
+
+  auto binsx = get_bin_edges1D(hfp.binning->bins, 0);
+  auto binsy = get_bin_edges1D(hfp.binning->bins, 1);
+  auto binsz = get_bin_edges1D(hfp.binning->bins, 2);
+
+  auto root_hist = std::make_unique<TH3D>(
+      name.c_str(), "", binsx.size() - 1, binsx.data(), binsy.size() - 1,
+      binsy.data(), binsz.size() - 1, binsz.data());
+
+  Eigen::ArrayXd bin_scales =
+      Eigen::ArrayXd::Constant(hfp.get_bin_contents().rows(), 1);
+  if (divide_by_bin_area) {
+    bin_scales = hfp.binning->bin_sizes();
+  }
+
+  Eigen::ArrayXd contents = hfp.get_bin_contents().col(col) / bin_scales;
+  Eigen::ArrayXd error = hfp.get_bin_uncertainty().col(col) / bin_scales;
+
+  for (size_t bix = 1; bix < binsx.size(); ++bix) {
+    for (size_t biy = 1; biy < binsy.size(); ++biy) {
+      for (size_t biz = 1; biz < binsz.size(); ++biz) {
+
+        auto bi = hfp.binning->find_bin({(binsx[bix - 1] + binsx[bix]) / 2.0,
+                                         (binsy[biy - 1] + binsy[biy]) / 2.0,
+                                         (binsz[biz - 1] + binsz[biz]) / 2.0});
+
+        if (bi == Binning::npos) {
+          std::stringstream ss;
+          ss << fmt::format("[ToTH3]: When looking for bin id [{},{},{}] = "
+                            "({},{}), ({},{}), ({},{})",
+                            bix - 1, biy - 1, biz - 1, binsx[bix - 1],
+                            binsx[bix], binsy[biy - 1], binsy[biy],
+                            binsz[biz - 1], binsz[biz])
+             << ". Failed to find it.\nInput hist:" << hf
+             << "\n  - Projected binning: " << hfp.binning->bins
+             << "\n  - bin edges x = " << fmt::format("{}", binsx)
+             << "\n  - bin edges y = " << fmt::format("{}", binsy)
+             << "\n  - bin edges z = " << fmt::format("{}", binsz) << std::endl;
+          log_critical(ss.str());
+          throw CatastrophicBinningFailure();
+        }
+
+        root_hist->SetBinContent(bix, biy, biz, contents(bi));
+        root_hist->SetBinError(bix, biy, biz, error(bi));
+      }
+    }
+  }
+  
+  return root_hist;
+}
 
 template <typename T> constexpr bool always_false_v = false;
 
@@ -182,11 +254,11 @@ template <typename TN> BinnedValues BinnedValues_from_ROOT(TN const &hist) {
 DECLARE_NUISANCE_EXCEPT(NonExistantInputROOTFile);
 DECLARE_NUISANCE_EXCEPT(NonExistantTH1);
 
-NuHepMC::GC7::EnergyDistribution get_EnergyDistribution_from_ROOT(
+NuHepMC::GC4::EnergyDistribution get_EnergyDistribution_from_ROOT(
     std::string const &fname, std::string const &hname,
     std::string const &energy_unit = "", bool is_per_width = true) {
-  NuHepMC::GC7::EnergyDistribution ed;
-  ed.dist_type = NuHepMC::GC7::EDistType::kHistogram;
+  NuHepMC::GC4::EnergyDistribution ed;
+  ed.dist_type = NuHepMC::GC4::EDistType::kHistogram;
   ed.MonoEnergeticEnergy = kMissingDatum<double>;
 
   TFile fin(fname.c_str());
--- src/nuis/python/pyConvert.cxx
+++ src/nuis/python/pyConvert.cxx
@@ -25,14 +25,74 @@ void pyConvertInit(py::module &m) {
            &get_EnergyDistribution_from_ROOT, py::arg("fname"),
            py::arg("hname"), py::arg("energy_unit") = std::string(""),
            py::arg("is_per_width") = false)
-      .def("write_TH1", [](std::string const &foutname,
-                           std::string const &hname, BinnedValues const &hf,
-                           bool divide_by_bin_width, std::string const &opts) {
-        TFile fout(foutname.c_str(), opts.c_str());
-        auto h = ToTH1(hf, hname, divide_by_bin_width);
-        fout.WriteTObject(h.get(), hname.c_str());
-        h->SetDirectory(nullptr);
-        fout.Write();
-        fout.Close();
-      });
+      .def("write_TH1",
+           [](std::string const &foutname, std::string const &hname,
+              BinnedValues const &hf, bool divide_by_bin_width,
+              std::string const &opts) {
+             TFile fout(foutname.c_str(), opts.c_str());
+             auto h = ToTH1(hf, hname, divide_by_bin_width);
+             fout.WriteTObject(h.get(), hname.c_str());
+             h->SetDirectory(nullptr);
+             fout.Write();
+             fout.Close();
+           })
+      .def(
+          "ToTH1",
+          [](BinnedValues const &hf, std::string const &name,
+             bool divide_by_bin_width, BinnedValuesBase::column_t col) {
+            return ToTH1(hf, name, divide_by_bin_width, col);
+          },
+          py::arg("hist"), py::arg("hname"), py::arg("divide_by_bin_width"),
+          py::arg("col") = 0)
+      .def(
+          "ToTH1",
+          [](HistFrame const &hf, std::string const &name,
+             bool divide_by_bin_width, BinnedValuesBase::column_t col) {
+            return ToTH1(hf, name, divide_by_bin_width, col);
+          },
+          py::arg("hist"), py::arg("hname"), py::arg("divide_by_bin_width"),
+          py::arg("col") = 0)
+      .def(
+          "ToTH2",
+          [](BinnedValues const &hf, std::string const &name,
+             bool divide_by_bin_width, BinnedValuesBase::column_t col) {
+            return ToTH2(hf, name, divide_by_bin_width, col);
+          },
+          py::arg("hist"), py::arg("hname"), py::arg("divide_by_bin_width"),
+          py::arg("col") = 0)
+      .def(
+          "ToTH2",
+          [](HistFrame const &hf, std::string const &name,
+             bool divide_by_bin_width, BinnedValuesBase::column_t col) {
+            return ToTH2(hf, name, divide_by_bin_width, col);
+          },
+          py::arg("hist"), py::arg("hname"), py::arg("divide_by_bin_width"),
+          py::arg("col") = 0)
+      .def(
+          "ToTH3",
+          [](BinnedValues const &hf, std::string const &name,
+             bool divide_by_bin_width, BinnedValuesBase::column_t col) {
+            return ToTH3(hf, name, divide_by_bin_width, col);
+          },
+          py::arg("hist"), py::arg("hname"), py::arg("divide_by_bin_width"),
+          py::arg("col") = 0)
+      .def(
+          "ToTH3",
+          [](HistFrame const &hf, std::string const &name,
+             bool divide_by_bin_width, BinnedValuesBase::column_t col) {
+            return ToTH3(hf, name, divide_by_bin_width, col);
+          },
+          py::arg("hist"), py::arg("hname"), py::arg("divide_by_bin_width"),
+          py::arg("col") = 0)
+      .def("write_TH3",
+           [](std::string const &foutname, std::string const &hname,
+              BinnedValues const &hf, bool divide_by_bin_width,
+              BinnedValuesBase::column_t col, std::string const &opts) {
+             TFile fout(foutname.c_str(), opts.c_str());
+             auto h = ToTH3(hf, hname, divide_by_bin_width, col);
+             fout.WriteTObject(h.get(), hname.c_str());
+             h->SetDirectory(nullptr);
+             fout.Write();
+             fout.Close();
+           });
 }
